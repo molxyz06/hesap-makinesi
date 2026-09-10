5816707580a1d23c15251385c7e0bfd52a2eb0e0
molxyz06 <molxyz06@gmail.com>
2026-09-10 21:35:25 +0300

    while dongusu eklendi ve else durumunda basa donmesi saglandi

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
-ilksayi = int(input("Ilk Sayinizi Giriniz: "))
-ikincisayi = int(input("Ikinci Sayinizi Giriniz: "))
-islem = str(input("""Yapmak Istediginiz Islemi Giriniz
+while True:
+    ilksayi = int(input("Ilk Sayinizi Giriniz: "))
+    ikincisayi = int(input("Ikinci Sayinizi Giriniz: "))
+    islem = str(input("""Yapmak Istediginiz Islemi Giriniz
 (Toplama: +, Cikarma: -, Carpma: x, Bolme: /) """))
 
 
+    if islem == "+":
+        print("Sonuc: " + str(ilksayi + ikincisayi))
+        break
 
-if islem == "+":
-    print(ilksayi + ikincisayi)
+    elif islem == "-":
+        print("Sonuc: " + str(ilksayi - ikincisayi))
+        break
 
-elif islem == "-":
-    print(ilksayi - ikincisayi)
+    elif islem == "x":
+        print("Sonuc: " + str(ilksayi * ikincisayi))
+        break
 
-elif islem == "x":
-    print(ilksayi * ikincisayi)
+    elif islem == "/":
+        print("Sonuc: " + str(int(ilksayi / ikincisayi)))
+        break
 
-elif islem == "/":
-    print(int(ilksayi / ikincisayi))
-
-else:
-    print("Isleminiz algilanamadi, lutfen tekrar deneyiniz.")
+    else:
+        print("Isleminiz algilanamadi, lutfen tekrar deneyiniz.")
+        continue
 
