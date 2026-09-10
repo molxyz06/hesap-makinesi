3d494d01c5493d6bcc953c37ba50e582cac5da0e
molxyz06 <molxyz06@gmail.com>
2026-09-10 21:45:02 +0300

    islem bittikten sonra kullanicinin yeni islem isteyip istemedigini soyleyebilecegi soru yoneltildi

--- main.py
+++ main.py
@@ -7,19 +7,39 @@ while True:
 
     if islem == "+":
         print("Sonuc: " + str(ilksayi + ikincisayi))
-        break
+
+        tekrarislem = input("Tekrar Islem Yapmak Ister Misiniz?(evet: e, hayir: h)")
+        if tekrarislem == "e":
+            continue
+        else:
+            break
 
     elif islem == "-":
         print("Sonuc: " + str(ilksayi - ikincisayi))
-        break
+
+        tekrarislem = input("Tekrar Islem Yapmak Ister Misiniz?(evet: e, hayir: h)")
+        if tekrarislem == "e":
+            continue
+        else:
+            break
 
     elif islem == "x":
         print("Sonuc: " + str(ilksayi * ikincisayi))
-        break
+
+        tekrarislem = input("Tekrar Islem Yapmak Ister Misiniz?(evet: e, hayir: h)")
+        if tekrarislem == "e":
+            continue
+        else:
+            break
 
     elif islem == "/":
         print("Sonuc: " + str(int(ilksayi / ikincisayi)))
-        break
+
+        tekrarislem = input("Tekrar Islem Yapmak Ister Misiniz?(evet: e, hayir: h)")
+        if tekrarislem == "e":
+            continue
+        else:
+            break
 
     else:
         print("Isleminiz algilanamadi, lutfen tekrar deneyiniz.")
